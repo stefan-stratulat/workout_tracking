f513677c5bc978c8a6b8e6d39dcf359cd084c542
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-23 06:54:51 +0200

    write data to google sheets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 
 #Input workout info
 workout = input("Tell me which exercesies you did: ")
@@ -16,4 +17,27 @@ parameters = {
 }
 
 response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", json=parameters, headers=headers)
-print(response.text)
+
+data = response.json()
+print(data)
+
+today = datetime.now()
+date = today.strftime("%Y-%m-%d")
+time = today.strftime("%H:%M")
+
+rows = {
+    "workout":
+        {"date":date,
+        "time":time,
+        "exercise":(data["exercises"][0]["name"]).title(),
+        "duration":data["exercises"][0]["duration_min"],
+        "calories": data["exercises"][0]["nf_calories"]
+        }
+}
+
+sheety_header ={
+    "Authorization":os.environ["SHEETY_API"]
+}
+
+add_rows = requests.post(url="https://api.sheety.co/7f2c9a5134d5a523511e869d5cf54025/myWorkouts/workouts",json=rows,headers=sheety_header)
+print(add_rows.status_code)
